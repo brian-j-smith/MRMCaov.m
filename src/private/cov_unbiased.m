627acb7429a0b56dbc72db4a4c80947f6cc1f13e
Brian J Smith <brian-j-smith@uiowa.edu>
2022-02-16 12:41:32 -0600

    Implement fast unbiased covariance algorithms

--- src/private/cov_unbiased.m
+++ src/private/cov_unbiased.m
@@ -12,10 +12,77 @@ function V = cov_unbiased(y, group, id, balanced)
   if balanced
     func = @cov_unbiased_balanced;
   else
-    func = @cov_unbiased_unbalanced;
+    func = @cov_unbiased_default;
   end
   V = func(y.truth, y.rating, group, id);
   
+end
+
+
+function V = cov_unbiased_default(truth, rating, group, id)
+
+  n = length(unique(group));
+  is_pos = truth == category(truth, 2);
+
+  V = zeros(n);
+  x = cell(n, 1);
+  for i = 1:n
+    x{i} = get_scores(rating, group == i, is_pos, id);
+    for j = 1:i
+      V(i, j) = get_cov(x{i}, x{j});
+      V(j, i) = V(i, j);
+    end
+  end
+
+end
+
+
+function res = get_scores(rating, is_group, is_pos, id)
+  is_group_pos = is_group & is_pos;
+  is_group_neg = is_group & ~is_pos;
+  res.id_pos = id(is_group_pos);
+  res.id_neg = id(is_group_neg);
+  res.scores = outerop(rating(is_group_pos), rating(is_group_neg), @c_score);
+  res.sum_scores_pos = sum(res.scores, 2);
+  res.sum_scores_neg = sum(res.scores, 1);
+  res.sum_scores = sum(res.sum_scores_neg);
+end
+
+
+function res = get_cov(x, y)
+
+  id_pos = intersect(x.id_pos, y.id_pos);
+  id_neg = intersect(x.id_neg, y.id_neg);
+  nxny = numel(x.scores) * numel(y.scores);
+  n_pos = length(id_pos);
+  n_neg = length(id_neg);
+
+  delta = nxny / (...
+    nxny -...
+    size(x.scores, 1) * n_neg * size(y.scores, 1) -...
+    size(x.scores, 2) * n_pos * size(y.scores, 2) +...
+    n_pos * n_neg...
+  );
+
+  [~, inds_pos] = ismember(id_pos, x.id_pos);
+  [~, inds_neg] = ismember(id_neg, x.id_neg);
+  x.sum_scores_pos = x.sum_scores_pos(inds_pos);
+  x.sum_scores_neg = x.sum_scores_neg(inds_neg);
+  x.scores = x.scores(inds_pos, inds_neg);
+  
+  [~, inds_pos] = ismember(id_pos, y.id_pos);
+  [~, inds_neg] = ismember(id_neg, y.id_neg);
+  y.sum_scores_pos = y.sum_scores_pos(inds_pos);
+  y.sum_scores_neg = y.sum_scores_neg(inds_neg);
+  y.scores = y.scores(inds_pos, inds_neg);
+  
+  res = (...
+    (1 - delta) * (x.sum_scores * y.sum_scores) +...
+    delta * (sum(x.sum_scores_pos .* y.sum_scores_pos) +...
+             sum(x.sum_scores_neg .* y.sum_scores_neg) -...
+             sum(x.scores .* y.scores, "all"))...
+  ) / nxny;
+
 end
 
 
@@ -25,118 +92,33 @@ function V = cov_unbiased_balanced(truth, rating, group, id)
   
   m = length(categories(id));
   n = length(rating) / m;
-  X = reshape(rating(sort_inds), m, n);
+  rating_mat = reshape(rating(sort_inds), m, n);
   truth = truth(sort_inds(1:m));
   
   is_pos = truth == category(truth, 2);
-  X_pos = X(is_pos, :);
-  X_neg = X(~is_pos, :);
-  n_pos = size(X_pos, 1);
-  n_neg = size(X_neg, 1);
+  rating_mat_pos = rating_mat(is_pos, :);
+  rating_mat_neg = rating_mat(~is_pos, :);
+  n_pos = size(rating_mat_pos, 1);
+  n_neg = size(rating_mat_neg, 1);
   
-  V = NaN(n);
-  scores = cell(n, 1);
-  for i = 1:n
-    x = outerop(X_pos(:, i), X_neg(:, i), @c_score);
-    scores{i} = x;
-    V(i, i) = var_unbiased(x, n_pos, n_neg);
-  end
+  scores = zeros(n_pos * n_neg, n);
+  sum_scores_pos = zeros(n_pos, n);
+  sum_scores_neg = zeros(n_neg, n);
   
   for i = 1:n
-    for j = 1:(i - 1)
-      x = scores{i} - scores{j};
-      var_diff = var_unbiased(x, n_pos, n_neg);
-      V(i, j) = (V(i, i) + V(j, j) - var_diff) / 2;
-      V(j, i) = V(i, j);
-    end
+    x = outerop(rating_mat_pos(:, i), rating_mat_neg(:, i), @c_score);
+    scores(:, i) = x(:);
+    sum_scores_pos(:, i) = sum(x, 2);
+    sum_scores_neg(:, i) = sum(x, 1);
   end
   
-end
-
-
-function v = var_unbiased(x, n_pos, n_neg)
-    q1 = sum(x.^2, "all");
-    m1 = q1 / (n_pos * n_neg);
-    q2 = sum(sum(x, 1).^2);
-    m2 = (q2 - q1) / (n_pos * n_neg * (n_neg - 1));
-    q3 = sum(sum(x, 2).^2);
-    m3 = (q3 - q1) / (n_neg * n_pos * (n_pos - 1));
-    q4 = sum(x, "all").^2;
-    m4 = (q4 - q2 - q3 + q1) / (n_neg * n_pos * (n_neg - 1) * (n_pos - 1));
-    v = m1 / (n_neg * n_pos) + m2 * (n_neg - 1) / (n_neg * n_pos) +...
-      m3 * (n_pos - 1) / (n_neg * n_pos) +...
-      m4 * ((n_neg - 1) * (n_pos - 1) / (n_neg * n_pos) - 1);
-end
-
-
-function V = cov_unbiased_unbalanced(truth, rating, group, id)
-
-  data = table(rating, group, id,...
-               'VariableNames', {'rating', 'group', 'id'});
-  n = length(unique(group));
-  
-  is_pos = truth == category(truth, 2);
-  data_pos = data(is_pos, :);
-  data_neg = data(~is_pos, :);
-  n_pos = length(unique(data_pos.id));
-  n_neg = length(unique(data_neg.id));
-  
-  scores = cell(n, 1);
-  for i = 1:n
-    scores{i} = group_c_scores(data_pos, data_neg, i);
-  end
-  
-  A1 = NaN(n);
-  A2 = NaN(n);
-  A3 = NaN(n);
-  A4 = NaN(n);
-
-  iter = 0;
-  niter = n * (n + 1) / 2;
-  wb = waitbar(0, 'Computing unbiased covariance matrix');
-  for i = 1:n
-    for j = 1:i
-      X = scores{i}.value * scores{j}.value';
-      same_id_pos = outerop(scores{i}.id_pos, scores{j}.id_pos, @eq);
-      same_id_neg = outerop(scores{i}.id_neg, scores{j}.id_neg, @eq);
-      
-      A1(i, j) = mean(X(same_id_pos & same_id_neg));
-      A1(j, i) = A1(i, j);
-      A2(i, j) = mean(X(same_id_pos & ~same_id_neg));
-      A2(j, i) = A2(i, j);
-      A3(i, j) = mean(X(~same_id_pos & same_id_neg));
-      A3(j, i) = A3(i, j);
-      A4(i, j) = mean(X(~same_id_pos & ~same_id_neg));
-      A4(j, i) = A4(i, j);
-      
-      iter = iter + 1;
-      waitbar(iter / niter, wb);
-    end
-  end
-  close(wb);
-  
-  V = (A1 + (n_neg - 1) * A2 + (n_pos - 1) * A3 +...
-       (1 - n_pos - n_neg) * A4) / (n_pos * n_neg);
-  
-end
-
-
-function scores = group_c_scores(data_pos, data_neg, which)
-
-  is_group_pos = data_pos.group == which;
-  rating_pos = data_pos.rating(is_group_pos);
-  id_pos = data_pos.id(is_group_pos);
-  
-  is_group_neg = data_neg.group == which;
-  rating_neg = data_neg.rating(is_group_neg);
-  id_neg = data_neg.id(is_group_neg);
-  
-  inds = combvec(1:length(rating_pos), 1:length(rating_neg))';
-  inds_pos = inds(:, 1);
-  inds_neg = inds(:, 2);
-  
-  scores.value = c_score(rating_pos(inds_pos), rating_neg(inds_neg));
-  scores.id_pos = id_pos(inds_pos);
-  scores.id_neg = id_neg(inds_neg);
+  delta = (n_neg * n_pos)^2 / (n_neg * (n_neg - 1) * n_pos * (n_pos - 1));
+  sum_scores = sum(scores, 1);
+  V = (...
+    (1 - delta) * (sum_scores' * sum_scores) +...
+    delta * (sum_scores_pos' * sum_scores_pos +...
+             sum_scores_neg' * sum_scores_neg -...
+             scores' * scores)...
+  ) / (n_neg * n_pos)^2;
   
 end
